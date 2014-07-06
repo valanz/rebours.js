f490373ea6221e268799ec54d26b80956b73e981
Valentin <valentin.menardie@gmail.com>
2014-07-06 18:55:04 +0200

    cs

--- src/rebours.js
+++ src/rebours.js
@@ -1,44 +1,44 @@
 function Rebours(view, target, origin) {
 
-	this.view   = view;
-	this.target = target;
-	this.origin = origin;
+    this.view   = view;
+    this.target = target;
+    this.origin = origin;
 
-	var self = this;
+    var self = this;
 
-	setInterval(function(){
-		self.getCountdown(self.target, self.origin, self.view);
-	}, 1000);
+    setInterval(function(){
+	    self.getCountdown(self.target, self.origin, self.view);
+    }, 1000);
 }
 
 Rebours.prototype.getCountdown = function(target, origin, view) {
 
-	var days,
-		hours,
-		minutes,
-		seconds,
-		t = new Date(target).getTime(),
-		o = new Date().getTime(),
-		v = document.getElementById(view),
-		total = (t - o) / 1000;
+    var days,
+	    hours,
+	    minutes,
+	    seconds,
+	    t = new Date(target).getTime(),
+	    o = new Date().getTime(),
+	    v = document.getElementById(view),
+	    total = (t - o) / 1000;
 
-	days  = parseInt(total / 86400);
-	total = total % 86400;
+    days  = parseInt(total / 86400);
+    total = total % 86400;
 
-	hours = parseInt(total / 3600);
-	total = total % 3600;
+    hours = parseInt(total / 3600);
+    total = total % 3600;
 
-	minutes = parseInt(total / 60);
-	seconds = parseInt(total % 60);
+    minutes = parseInt(total / 60);
+    seconds = parseInt(total % 60);
 	
-	if (hours >= 0) {
-		v.innerHTML = days + "J " +
-		hours + "H " +
-		minutes + "M " +
-		seconds + "S";
-	}
+    if (hours >= 0) {
+	    v.innerHTML = days + "J " +
+	    hours + "H " +
+	    minutes + "M " +
+	    seconds + "S";
+    }
 
-	else {
-		v.innerHTML = "Countdown is over";
-	}
+    else {
+	    v.innerHTML = "Countdown is over";
+    }
 };
