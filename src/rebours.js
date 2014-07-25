b2cf28561e959a0f172cc84f00f9ac3945d16350
Valentin <valentin.menardie@gmail.com>
2014-07-25 17:13:53 +0200

    cs

--- src/rebours.js
+++ src/rebours.js
@@ -7,38 +7,38 @@ function Rebours(view, target, origin) {
     var self = this;
 
     setInterval(function(){
-	    self.getCountdown(self.target, self.origin, self.view);
+        self.getCountdown(self.target, self.origin, self.view);
     }, 1000);
 }
 
 Rebours.prototype.getCountdown = function(target, origin, view) {
 
     var days,
-	    hours,
-	    minutes,
-	    seconds,
-	    t = new Date(target).getTime(),
-	    o = new Date().getTime(),
-	    v = document.getElementById(view),
-	    total = (t - o) / 1000;
+        hours,
+        minutes,
+        seconds,
+        t = new Date(target).getTime(),
+        o = new Date().getTime(),
+        v = document.getElementById(view),
+        total = (t - o) / 1000;
 
-    days  = parseInt(total / 86400);
+    days  = parseInt(total / 86400, null);
     total = total % 86400;
 
-    hours = parseInt(total / 3600);
+    hours = parseInt(total / 3600, null);
     total = total % 3600;
 
-    minutes = parseInt(total / 60);
-    seconds = parseInt(total % 60);
+    minutes = parseInt(total / 60, null);
+    seconds = parseInt(total % 60, null);
 	
     if (hours >= 0) {
-	    v.innerHTML = days + "J " +
-	    hours + "H " +
-	    minutes + "M " +
-	    seconds + "S";
+        v.innerHTML = days + "J " +
+        hours + "H " +
+        minutes + "M " +
+        seconds + "S";
     }
 
     else {
-	    v.innerHTML = "Countdown is over";
+        v.innerHTML = "Countdown is over";
     }
 };
